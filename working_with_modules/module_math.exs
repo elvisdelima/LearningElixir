51bc88bb77468ef6c63ba45eff93f8a57010c069
Elvis Lima <elvislima@MacBook-Pro-do-Elvis.local>
2020-04-07 22:30:52 -0300

    working with import, alias and require diretives

---
+++ working_with_modules/module_math.exs
@@ -0,0 +1,5 @@
+defmodule ModuleMath do
+  def sum(param1, param2) do
+    param1 + param2
+  end
+end
--- working_with_modules/module_playground.exs
+++ working_with_modules/module_playground.exs
@@ -1,7 +1,24 @@
 defmodule ModulePlayground do
 	import IO, only: [puts: 1]
+	import Kernel, except: [inspect: 1]
+	alias ModuleMath, as: MyMath
+	require Integer
 
 	def say_hello do
-		puts "Hello 2!"
+		inspect "Hello 2!"
+	end
+
+	def inspect(param1) do
+		puts "Starting Output"
+		puts param1
+		puts "Ending Output"
+	end
+
+	def print_sum do
+		MyMath.sum(1,2)
+	end
+
+	def print_is_even(num) do
+		puts "Is #{num} even? #{Integer.is_even(num)}"
 	end
 end
